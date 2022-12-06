49c7187f54cd242fb696d6cfa5bfa8898aa45af7
Hristina Adamovic <hristinacina@gmail.com>
2022-12-06 22:25:49 +0100

    added mapper dependency

--- src/main/java/com/example/test/controller/DriverController.java
+++ src/main/java/com/example/test/controller/DriverController.java
@@ -6,6 +6,7 @@ import com.example.test.domain.user.Driver;
 import com.example.test.domain.user.Document;
 import com.example.test.domain.vehicle.Vehicle;
 import com.example.test.service.interfaces.IDriverService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
