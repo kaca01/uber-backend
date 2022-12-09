4e6f6498165d094c2ea86dc821ea49d31098b31c
savic-a <anastasijas557@gmail.com>
2022-12-09 21:06:28 +0100

    Added unregisteredDTO object into controller

--- src/main/java/com/example/test/controller/UnregisteredUserController.java
+++ src/main/java/com/example/test/controller/UnregisteredUserController.java
@@ -1,7 +1,6 @@
 package com.example.test.controller;
 
-import com.example.test.domain.ride.Location;
-import com.example.test.domain.vehicle.Vehicle;
+import com.example.test.dto.UnregisteredUserDTO;
 import com.example.test.service.interfaces.IUnregisteredUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/unregisteredUser/")
@@ -18,14 +17,17 @@ public class UnregisteredUserController {
     @Autowired
     private IUnregisteredUserService service;
 
+    // Getting the assumption about the time and cost of the ride
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<Double>> createGreeting(@RequestBody Collection<Location> locations, @RequestBody Vehicle vehicle)
+    public ResponseEntity<UnregisteredUserDTO> createGreeting(@RequestBody UnregisteredUserDTO unregisteredUserDTO)
             throws Exception {
-        Collection<Double> estimatedValues = service.getEstimationTimeAndCost(locations, vehicle);
+
+        // the data is not stored in the database, so we can pass the dto object to the service
+        List<Double> estimatedValues = service.getEstimationTimeAndCost(unregisteredUserDTO);
 
         if(estimatedValues == null) {
-            return new ResponseEntity<Collection<Double>>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<Collection<Double>>(estimatedValues, HttpStatus.CREATED);
+        return new ResponseEntity<>(new UnregisteredUserDTO(estimatedValues.get(0), estimatedValues.get(1)), HttpStatus.CREATED);
     }
 }
