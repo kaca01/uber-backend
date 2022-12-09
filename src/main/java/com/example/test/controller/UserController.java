e92a67566f3e5532baa3a5a6dbbd6d0e4f1e8a3c
savic-a <anastasijas557@gmail.com>
2022-12-09 21:07:03 +0100

    Added userDTO objects into controller

--- src/main/java/com/example/test/controller/UserController.java
+++ src/main/java/com/example/test/controller/UserController.java
@@ -4,14 +4,20 @@ import com.example.test.domain.communication.Message;
 import com.example.test.domain.communication.Note;
 import com.example.test.domain.ride.Ride;
 import com.example.test.domain.user.User;
+import com.example.test.dto.*;
 import com.example.test.service.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.awt.print.Pageable;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -20,68 +26,105 @@ public class UserController {
     @Autowired
     private IUserService service;
 
+    // Rides of the user
     @GetMapping(value ="/user/{id}/ride", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<Ride>> getRides(@PathVariable Long id) {
-        Collection<Ride> rides = service.getRides(id);
+    public ResponseEntity<AllRidesDTO> getRides(@PathVariable int id, @RequestParam int page, @RequestParam int size,
+                                                @RequestParam String sort, @RequestParam String from,
+                                                @RequestParam String to) {
+
+        Pageable pageable = (Pageable) PageRequest.of(page, size, Sort.by(sort));
+        List<Ride> rides = service.getRides((long) id, pageable, from, to);
 
         if(rides == null) {
-            return new ResponseEntity<Collection<Ride>>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        List<RideDTO> ridesDTO = new ArrayList<>();
+        for (Ride ride : rides) {
+            ridesDTO.add(new RideDTO(ride));
         }
         // todo za 400
-        return new ResponseEntity<Collection<Ride>>(rides, HttpStatus.OK);
+        return new ResponseEntity<>(new AllRidesDTO(ridesDTO.size(), ridesDTO), HttpStatus.OK);
     }
 
+    // Getting multiple of them for the reason of showing a list
     @GetMapping(value ="/user", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> get() {
-        User user = service.get();
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+    public ResponseEntity<AllUsersDTO> get(@RequestParam int page, @RequestParam int size) {
+
+        Pageable pageable = (Pageable) PageRequest.of(page, size);
+        Page<User> users = service.get(pageable);
+
+        List<UserDTO> usersDTO = new ArrayList<>();
+        for(User user : users) {
+            usersDTO.add(new UserDTO(user));
+        }
+        return new ResponseEntity<>(new AllUsersDTO(usersDTO.size(), usersDTO), HttpStatus.OK);
     }
 
+    // TODO
+    // login
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Boolean> login(@RequestBody String email, @RequestBody String password) throws Exception {
-        Boolean success = service.login(email, password);
+    public ResponseEntity<Boolean> login(@RequestBody LoginDTO loginDTO) throws Exception {
+
+        Boolean success = service.login(loginDTO.getEmail(), loginDTO.getPassword());
 
         if(success == null) {
-            return new ResponseEntity<Boolean>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<Boolean>(success, HttpStatus.CREATED);
     }
 
+    // Returns a list of user messages
     @GetMapping(value ="/user/{id}/message", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<Message>> getMessages(@PathVariable Long id) {
-        Collection<Message> messages = service.getMessages(id);
+    public ResponseEntity<AllMessagesDTO> getMessages(@PathVariable int id) {
+
+        List<Message> messages = service.getMessages((long) id);
 
         if(messages == null) {
-            return new ResponseEntity<Collection<Message>>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+
+        List<MessageDTO> messagesDTO = new ArrayList<>();
+        for(Message message : messages) {
+            messagesDTO.add(new MessageDTO(message));
+        }
+
         // todo za 400
-        return new ResponseEntity<Collection<Message>>(messages, HttpStatus.OK);
+        return new ResponseEntity<>(new AllMessagesDTO(messagesDTO.size(), messagesDTO), HttpStatus.OK);
     }
 
+    // Send a message to the user
     @PostMapping(value = "/user/{id}/message", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Message> insertMessage(@PathVariable Long id, @RequestBody Message requestMessage) throws Exception {
-        Message message = service.insertMessage(id, requestMessage);
+    public ResponseEntity<MessageDTO> insertMessage(@PathVariable int id, @RequestBody MessageDTO messageDTO) throws Exception {
+
+        Message message = new Message(messageDTO);
+        message = service.insertMessage((long) id, message);
 
         if(message == null) {
-            return new ResponseEntity<Message>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+
         // todo za 400
-        return new ResponseEntity<Message>(message, HttpStatus.CREATED);
+        return new ResponseEntity<>(new MessageDTO(message), HttpStatus.CREATED);
     }
 
+    // Blocking the user from the part of the administrator
     @PutMapping(value = "/user/{id}/block", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Boolean> block(@PathVariable Long id) throws Exception {
-        Boolean blockedUser = service.block(id);
+    public ResponseEntity<Boolean> block(@PathVariable int id) throws Exception {
+
+        Boolean blockedUser = service.block((long) id);
 
         if (blockedUser == null) {
-            return new ResponseEntity<Boolean>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<Boolean>(blockedUser, HttpStatus.OK);
+        return new ResponseEntity<>(blockedUser, HttpStatus.OK);
     }
 
+    // Unblocking user from the administrator
     @PutMapping(value = "/user/{id}/unblock", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Boolean> unblock(@PathVariable Long id) throws Exception {
-        Boolean unblockedUser = service.unblock(id);
+    public ResponseEntity<Boolean> unblock(@PathVariable int id) throws Exception {
+
+        Boolean unblockedUser = service.unblock((long) id);
 
         if (unblockedUser == null) {
             return new ResponseEntity<Boolean>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -89,25 +132,37 @@ public class UserController {
         return new ResponseEntity<Boolean>(unblockedUser, HttpStatus.OK);
     }
 
+    // Creating note
     @PostMapping(value = "/user/{id}/note", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Note> insertNote(@PathVariable Long id, @RequestBody Note requestNote) throws Exception {
-        Note note = service.insertNote(id, requestNote);
+    public ResponseEntity<NoteDTO> insertNote(@PathVariable int id, @RequestBody NoteDTO requestNote) throws Exception {
+
+        Note note = new Note(requestNote);
+        note = service.insertNote((long) id, note);
 
         if(note == null) {
-            return new ResponseEntity<Note>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         // todo za 400
-        return new ResponseEntity<Note>(note, HttpStatus.CREATED);
+        return new ResponseEntity<>(new NoteDTO(note), HttpStatus.CREATED);
     }
 
+    // Getting notes for the user
     @GetMapping(value ="/user/{id}/note", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<Note>> getNotes(@PathVariable Long id) {
-        Collection<Note> notes = service.getNotes(id);
+    public ResponseEntity<AllNotesDTO> getNotes(@PathVariable int id, @RequestParam int page,
+                                                @RequestParam int size) {
+
+        Pageable pageable = (Pageable) PageRequest.of(page, size);
+        List<Note> notes = service.getNotes((long) id, pageable);
 
         if(notes == null) {
-            return new ResponseEntity<Collection<Note>>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        List<NoteDTO> notesDTO = new ArrayList<>();
+        for(Note note : notes) {
+            notesDTO.add(new NoteDTO(note));
         }
         // todo za 400
-        return new ResponseEntity<Collection<Note>>(notes, HttpStatus.OK);
+        return new ResponseEntity<>(new AllNotesDTO(notesDTO.size(), notesDTO), HttpStatus.OK);
     }
 }
