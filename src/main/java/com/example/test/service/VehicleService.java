0c64040a7243550bb0e90ec3f4daef4071e11ca4
savic-a <anastasijas557@gmail.com>
2022-12-11 02:31:51 +0100

    Removed mockup data

--- src/main/java/com/example/test/service/VehicleService.java
+++ src/main/java/com/example/test/service/VehicleService.java
@@ -1,19 +1,18 @@
 package com.example.test.service;
 
+import com.example.test.Mockup;
 import com.example.test.domain.ride.Location;
 import com.example.test.domain.vehicle.Vehicle;
-import com.example.test.domain.vehicle.VehicleType;
-import com.example.test.enumeration.VehicleTypeName;
 import com.example.test.service.interfaces.IVehicleService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class VehicleService implements IVehicleService {
 
-    private List<Vehicle> vehicles = creatVehicle();
+    Mockup mockup = new Mockup();
+    List<Vehicle> vehicles = mockup.vehicles;
 
     @Override
     public Boolean update(Long id, Location location) {
@@ -29,20 +28,5 @@ public class VehicleService implements IVehicleService {
             if(v.getId().equals(id)) return v;
         }
         return null;
-    }
-
-    // creating dummy data
-    private List<Vehicle> creatVehicle()
-    {
-        Vehicle v1 = new Vehicle(1L, new VehicleType(1L, VehicleTypeName.STANDARD, 42), "opel 21", "U3dhZ2dlciByb2Nrcw==", 2, new Location(1L, 24.24, 27.27, "Petra Perica 33"), true, false);
-        Vehicle v2 = new Vehicle(2L, new VehicleType(1L, VehicleTypeName.STANDARD, 42), "opel 21", "U3dhZ2dlciByb2Nrcw==", 2, new Location(1L, 24.24, 27.27, "Petra Perica 33"), true, false);
-        Vehicle v3 = new Vehicle(3L, new VehicleType(1L, VehicleTypeName.STANDARD, 42), "opel 21", "U3dhZ2dlciByb2Nrcw==", 2, new Location(1L, 24.24, 27.27, "Petra Perica 33"), true, false);
-        Vehicle v4 = new Vehicle(4L, new VehicleType(1L, VehicleTypeName.STANDARD, 42), "opel 21", "U3dhZ2dlciByb2Nrcw==", 2, new Location(1L, 24.24, 27.27, "Petra Perica 33"), true, false);
-        List<Vehicle> ps = new ArrayList<>();
-        ps.add(v1);
-        ps.add(v2);
-        ps.add(v3);
-        ps.add(v4);
-        return ps;
     }
 }
