9ffba65005436fdf02c6f6da8b091f4ee4128f6e
savic-a <anastasijas557@gmail.com>
2022-12-23 21:19:05 +0100

    Added that admin extends user

--- src/main/java/com/example/test/domain/user/Admin.java
+++ src/main/java/com/example/test/domain/user/Admin.java
@@ -1,26 +1,14 @@
 package com.example.test.domain.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Entity
-public class Admin{
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "username", unique = true, nullable = false)
-    private String username;
-    @Column(name = "password", nullable = false)
-    private String password;
-    @Column(name = "name", nullable = false)
-    private String name;
-    @Column(name = "lastname", nullable = false)
-    private String lastname;
-    @Column(name = "profilePicture", nullable = true)
-    private String profilePicture;
+//@PrimaryKeyJoinColumn(name = "id")
+public class Admin extends User {
+
 }
