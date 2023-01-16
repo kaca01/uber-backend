b0d89049880274fad8c6b7c1e05bc2d9a8f0b24b
Hristina Adamovic <hristinacina@gmail.com>
2023-01-16 23:09:32 +0100

    added ride exception handling

--- src/main/java/com/example/test/repository/ride/IRideRepository.java
+++ src/main/java/com/example/test/repository/ride/IRideRepository.java
@@ -9,14 +9,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.Nullable;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IRideRepository extends JpaRepository<Ride, Long> {
 
     public List<Ride> findByPassengers_id(Long passengerId);
 
-    public Ride findByStatusAndDriver_id(RideStatus status, Long driverId);
+    public Optional<Ride> findByStatusAndDriver_id(RideStatus status, Long driverId);
 
-    public Ride findByStatusAndPassengers_id(RideStatus status, Long passengerId);
+    public Optional<Ride> findByStatusAndPassengers_id(RideStatus status, Long passengerId);
+    public List<Ride> findRidesByStatusAndPassengers_email(RideStatus status, String email);
 
     public List<Ride> findRidesByDriverId(Long id);
 
