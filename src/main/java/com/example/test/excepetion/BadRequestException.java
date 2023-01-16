67567a324288ce6db4b6a7a8801656c3c1f9076d
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-01-16 19:07:50 +0100

    Added exceptions

---
+++ src/main/java/com/example/test/excepetion/BadRequestException.java
@@ -0,0 +1,12 @@
+package com.example.test.excepetion;
+
+@SuppressWarnings("serial")
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException() {}
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
+
