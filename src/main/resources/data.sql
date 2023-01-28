dda79fe9820fdf82a4fec086a0bd846ae1d6448e
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-01-28 07:42:36 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -81,7 +81,7 @@ insert into rejection(reason, time_of_rejection, user_id) values ('Gave me some 
 
 insert into ride(start_time, end_time, total_cost, estimated_time_in_minutes, status, baby_transport, pet_transport,
                  driver_id, rejection_id, vehicle_id, scheduled_time)
-values ('2023-01-13T16:20:24.893Z', '2023-01-22T16:40:24.893Z', 550.45, 190, 4, false, false, 5, 1, 1, '2023-01-13T16:20:24.893Z');
+values ('2023-01-12T16:20:24.893Z', '2023-01-22T16:40:24.893Z', 550.45, 190, 4, false, false, 5, 1, 1, '2023-01-13T16:20:24.893Z');
 insert into ride(start_time, end_time, total_cost, estimated_time_in_minutes, status, baby_transport, pet_transport,
                  driver_id, rejection_id, vehicle_id, scheduled_time)
 values ('2023-01-13T16:00:24.893Z', '2023-01-23T16:20:24.893Z', 400.53, 180.89, 4, false, false, 6, 2, 2, '2023-01-13T16:20:24.893Z');
@@ -99,7 +99,7 @@ insert into ride(start_time, end_time, total_cost, estimated_time_in_minutes, st
 values ('2022-12-22T13:42:24.893Z', null, 350, 7, 1, false, false, 6, 2, 1, '2023-01-13T16:20:24.893Z');
 insert into ride(start_time, end_time, total_cost, estimated_time_in_minutes, status, baby_transport, pet_transport,
                  driver_id, rejection_id, vehicle_id, scheduled_time)
-values ('2022-12-22T13:42:24.893Z', null, 350, 7, 0, false, false, 5, 2, 1, '2023-01-13T16:20:24.893Z');
+values ('2022-12-22T13:42:24.893Z', null, 350, 7, 1, false, false, 5, 2, 1, '2023-01-13T16:20:24.893Z');
 insert into ride(start_time, end_time, total_cost, estimated_time_in_minutes, status, baby_transport, pet_transport,
                  driver_id, rejection_id, vehicle_id, scheduled_time)
 values ('2022-12-22T13:42:24.893Z', null, 350, 7, 3, false, false, 8, 1, 2, '2023-01-13T16:20:24.893Z');
