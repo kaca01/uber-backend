b644b2221a2ebcbbde54857fe4ea9e279072dcaf
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-01-31 17:06:54 +0100

    Added socket theme and endpoint

--- src/main/java/com/example/test/configuration/WebSocketConfig.java
+++ src/main/java/com/example/test/configuration/WebSocketConfig.java
@@ -16,14 +16,24 @@ import java.util.Collections;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     //dodavanje tema
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        //registry.enableSimpleBroker("/notification");  //teme
+        registry.enableSimpleBroker("/notification");  //teme
         registry.enableSimpleBroker("/map-updates");  //teme
         //registry.setApplicationDestinationPrefixes("/app"); //kada front salje apije, slace sa ovim prefiksom
+
+        registry.setApplicationDestinationPrefixes("/socket-subscriber") // Prefiks koji koji se koristi za mapiranje svih poruka.
+                // Klijenti moraju da ga navedu kada salju poruku serveru.
+                // Svaki URL bi pocinjao ovako: http://localhost:8080/socket-subscriber/…/…
+                .enableSimpleBroker("/socket-publisher"); // Definisanje topic-a (ruta) na koje klijenti mogu da se pretplate.
+        // SimpleBroker cuva poruke u memoriji i salje ih klijentima na definisane topic-e.
+        // Server kada salje poruke, salje ih na rute koje su ovde definisane, a klijenti cekaju na poruke.
+        // Vise ruta odvajamo zarezom, npr. enableSimpleBroker("/ruta1", "/ruta2");
+
     }
 
     // otvaranje konekcije
@@ -32,9 +42,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/socket")
                 .setAllowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
                 .withSockJS();
-/*        registry.addEndpoint("/notifications")
+        registry.addEndpoint("/notification")
                 .setAllowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
-                .withSockJS();*/
+                .withSockJS();
+
+        registry.addEndpoint("/notif") // Definisemo endpoint koji ce klijenti koristiti da se povezu sa serverom.
+                // U ovom slucaju, URL za konekciju ce biti http://localhost:8080/socket/
+                .setAllowedOrigins("http://localhost:4200", "http://127.0.0.1:4200") // Dozvoljavamo serveru da prima zahteve bilo kog porekla
+                .withSockJS(); // Koristi se SockJS: https://github.com/sockjs/sockjs-protocol
     }
 
     @Bean
