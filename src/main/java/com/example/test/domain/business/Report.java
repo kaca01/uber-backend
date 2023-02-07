da67a35624f8d10bcf0d7464949ed9de67d55b13
savic-a <anastasijas557@gmail.com>
2023-02-07 05:21:05 +0100

    Renamed report attribute

--- src/main/java/com/example/test/domain/business/Report.java
+++ src/main/java/com/example/test/domain/business/Report.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Report {
-    private String date;
+    private String name;
     private double value;
 }
