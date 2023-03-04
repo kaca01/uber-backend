346b67878ce1b8ad618b03ec7d89a4e60f84b80d
Hristina Adamovic <hristinacina@gmail.com>
2023-03-04 11:57:12 +0100

    restored review logic

--- src/main/java/com/example/test/repository/ride/IRideRepository.java
+++ src/main/java/com/example/test/repository/ride/IRideRepository.java
@@ -26,5 +26,7 @@ public interface IRideRepository extends JpaRepository<Ride, Long> {
 
     public List<Ride> findRidesByStatusAndDriver_Id(RideStatus status, Long driverId);
     public List<Ride> findRidesByStatusAndPassengers_Id(RideStatus status, Long driverId);
+    @Query("select r.reviews from Ride r where r.id = ?1")
+    public List<Review> findReviewsByRideId(Long id);
     List<Ride> findByStatus(RideStatus status);
 }
